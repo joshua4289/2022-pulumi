518d3e24e4333a14960bde81cc7353740b8e199e
josh <joshua.jude.lobo@gmail.com>
2023-10-07 20:15:01 +0100

    update to London

--- basic_project/__main__.py
+++ basic_project/__main__.py
@@ -11,7 +11,7 @@ def main() -> None:
         "my-bucket",
         website=storage.BucketWebsiteArgs(main_page_suffix="index.html"),
         uniform_bucket_level_access=True,
-        location="UK",
+        location="EUROPE-WEST2",
         force_destroy=True,
     )
 
