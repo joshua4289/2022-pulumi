e17e42a0481e8a826ee5977cbd2b05d4e94ce737
josh <joshua.jude.lobo@gmail.com>
2023-10-07 19:21:54 +0100

    the commands for setup of gcloud cli

---
+++ basic_project/commands.sh
@@ -0,0 +1,12 @@
+# this is so I don't forget what I did
+# install gcloud SDK in bash
+
+
+# gcloud is already configured in bash
+
+# create a project
+gcloud projects create first-project-fastapi
+# get the default project
+gcloud config get project
+# login setup for pulumi
+gcloud default application_login
